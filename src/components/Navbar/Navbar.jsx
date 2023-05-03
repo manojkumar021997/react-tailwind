19e0a622b1b186bd5dee148ba080bfaeb906100e
kesavan <arunprakash7871@gmail.com>
2023-05-03 06:38:55 -0400

    changed navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
 const Navbar =()=>{
     return(
-        <div className="flex justify-between items-center p-[1rem] ">
-            <div className="cursor-pointer">
+        <div className="flex justify-between items-center p-[1rem] shadow-md ">
+            <div className="flex items-center justify-between gap-4 cursor-pointer ">
                 <p>
-                    <strong className="text-[25px] font-semibold text-yellow-500">Bumble</strong>
-                    <span className="text-[25px]">bee</span></p>
+                    <strong className="text-[25px] font-semibold text-yellowColor">Bumble</strong>
+                    <span className="text-[25px] text-blackColor font-semibold">bee</span>
+                </p>
+                <ul className="flex gap-4 cursor-pointer font-semibold">
+                    <li className="hover:text-yellowColor">Home</li>
+                    <li className="hover:text-yellowColor">About</li>
+                    <li className="hover:text-yellowColor">Contact</li>
+                </ul>
             </div>
             <div>
-                <ul className=" flex gap-4 cursor-pointer">
-                    <li className="hover:text-yellow-500">Signin</li>
-                    <li className="hover:text-yellow-500">Signup</li>
-                    <li className="hover:text-yellow-500">Profile</li>
+                <ul className=" flex gap-4 cursor-pointer font-semibold">
+                    <li className="hover:text-yellowColor">Signin</li>
+                    <li className="hover:text-yellowColor">Signup</li>
+                    <li className="hover:text-yellowColor">Profile</li>
                 </ul>
             </div>
         </div>
