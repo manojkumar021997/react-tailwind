9dd05e6e0b9cb04adb48b16cbadf8cdd29d64f10
kesavan <arunprakash7871@gmail.com>
2023-05-03 02:57:47 -0400

    Added navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,19 @@
 const Navbar =()=>{
     return(
-        <h1>Navbar</h1>
+        <div className="flex justify-between items-center p-[1rem] ">
+            <div className="cursor-pointer">
+                <p>
+                    <strong className="text-[25px] font-semibold text-yellow-500">Bumble</strong>
+                    <span className="text-[25px]">bee</span></p>
+            </div>
+            <div>
+                <ul className="flex">
+                    <li>Signin</li>
+                    <li>Signup</li>
+                    <li>Profile</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 export default Navbar;
