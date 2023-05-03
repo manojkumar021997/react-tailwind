56a0580482e7665fe25f4a7ca98f0c24bb299af3
kesavan <arunprakash7871@gmail.com>
2023-05-03 01:56:05 -0400

    Added Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+import Navbar from './components/Navbar/Navbar'
 
 const App = () =>{
   return(
-    <div><h1 className="text-blue-500 font-bold text-3xl">hello</h1></div>
+    <div>
+      <Navbar/>
+      <h1 className="text-blue-500 font-bold text-3xl">hello</h1>
+      </div>
   )
 
   
