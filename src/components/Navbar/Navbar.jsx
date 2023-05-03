336e4aad2118aae5e5aca5b169dbcbab87fab110
kesavan <arunprakash7871@gmail.com>
2023-05-03 03:40:29 -0400

    added navbar property

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar =()=>{
                     <span className="text-[25px]">bee</span></p>
             </div>
             <div>
-                <ul className="flex">
-                    <li>Signin</li>
-                    <li>Signup</li>
-                    <li>Profile</li>
+                <ul className="flex gap-4 cursor-pointer">
+                    <li className="hover:text-yellow-500">Signin</li>
+                    <li className="hover:text-yellow-500">Signup</li>
+                    <li className="hover:text-yellow-500">Profile</li>
                 </ul>
             </div>
         </div>
