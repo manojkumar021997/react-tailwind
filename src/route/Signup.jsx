5af05d6e060d4125befac7c9dfa0d5b7a2092dba
kesavan <arunprakash7871@gmail.com>
2023-05-05 04:28:45 -0400

    created signin and signup page

--- src/route/Signup.jsx
+++ src/route/Signup.jsx
@@ -1,11 +1,21 @@
+import bgimage from '../assets/images/bumblebee.png'
 const Singnup = () => {
     return(
-        <div className="grid grid-cols-2 text-shdowColor">
-            <div>
-                <p>img</p>
+        <div className="grid grid-cols-2 w-[96vw] h-[90vh] text-shdowColor">
+            <div  className="bg-yellowColor opacity-70  flex items-center justify-center ">
+                <div className='bg-yellowColor w-[60%]'>
+                    <h1 className='bg-transparent text-blackColor text-start font-semibold text-[30px]'>We are  </h1>
+                    <h2 className='text-[30px] font-semibold text-center bg-transparent text-blackColor'>Work Like</h2>
+                    <div className='text-end bg-yellowColor'>
+                        <strong className=" text-[25px] text font-semibold text-yellowColor">Bumble</strong>
+                        <span className="text-[25px] text-shdowColor opacity-100 font-semibold">bee</span>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div>google</div>
+            <div style={{backgroundImage: `url(${bgimage})`}} className='flex flex-col bg-cover opacity-50 bg-center bg-no-repeat justify-center items-center'>
+                <div className='bg-transparent text-whiteColor'>
+                    <button className='opacity-100'>Signin With Google</button>
+                </div>
                 <div>details</div>
             </div>
         </div>
--- src/route/Singin.jsx
+++ src/route/Singin.jsx
@@ -1,6 +1,27 @@
+import bgimage from '../assets/images/bumblebee.png'
 const Singnin = () => {
     return(
-        <div>Singin</div>
+        <div>
+            <div className="grid grid-cols-2 max-w-[100vw] h-[90vh] text-shdowColor">
+                
+                <div style={{ backgroundImage: `url(${bgimage})` }} className='flex flex-col bg-cover opacity-50 bg-center bg-no-repeat justify-center items-center'>
+                    <div className='bg-transparent text-whiteColor'>
+                        <button className='opacity-100'>Signin With Google</button>
+                    </div>
+                    <div>details</div>
+                </div>
+                <div className="bg-yellowColor opacity-70  flex items-center justify-center ">
+                    <div className='bg-yellowColor w-[60%]'>
+                        <h1 className='bg-transparent text-blackColor text-start font-semibold text-[30px]'>We are  </h1>
+                        <h2 className='text-[30px] font-semibold text-center bg-transparent text-blackColor'>Work Like</h2>
+                        <div className='text-end bg-yellowColor'>
+                            <strong className=" text-[25px] text font-semibold text-yellowColor">Bumble</strong>
+                            <span className="text-[25px] text-shdowColor opacity-100 font-semibold">bee</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
