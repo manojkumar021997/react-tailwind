ed79ee192d26b0a84100c86957c662a5c75ab45e
kesavan <arunprakash7871@gmail.com>
2023-05-08 07:32:47 -0400

    Added img

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -1,7 +1,11 @@
 import Card from "../components/Card/Card"
+import JobCard from "../components/JobCard/JobCard"
 const Home = () =>{
     return(
+       <>
        <Card/>
+       <JobCard/>
+       </>
     )
 }
 
