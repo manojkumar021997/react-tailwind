3d0c3835646c713b2a6c5e8782960d8f64f19d87
kesavan <arunprakash7871@gmail.com>
2023-05-08 03:56:15 -0400

    Added Card Components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,24 @@
 import React from 'react' 
 import person_1 from '../../assets/images/per1.png'
-import person_2 from '../../assets/images/per2.png'
-import person_3 from '../../assets/images/per3.png'
+// import person_2 from '../../assets/images/per2.png'
+// import person_3 from '../../assets/images/per3.png'
 
 
 const Card = () => {
   return (
-    <div className='grid grid-cols-2'>
-        <div className='w-[100%] h-[80vh] flex items-center justify-center -space-x-5 overflow-hidden'>
-              <img className='w-[120px] rounded-full ' src={person_2} alt='per2' />
-              <img className='w-[120px] rounded-full mt-[4rem] ' src={person_1} alt='per1' />
-              <img className='w-[120px] rounded-full mt-[8rem]' src={person_3} alt='per3' />
+    <div className='grid grid-cols-2 '>
+        <div className='text-shdowColor  flex flex-col justify-center items-center gap-[1rem]'>
+             <h2 className='w-[80%] text-[35px]'>
+                <strong >As we express our gratitude,</strong> 
+                we must never forget that the highest appreciation is not to utter words, but to live by them.
+             </h2>
+             <form className='w-[80%] flex -space-x-2 '>
+                <input type='text' className='bg-white w-[50%] text-shdowcolor  rounded-md py-2'/>
+                <button className='bg-yellowColor text-blackColor text-[20px] rounded-lg'>Search Jobs</button>
+             </form>
         </div>
-        <div>
-
+        <div className='flex items-center justify-center'>
+            <img className='w-[300px] rounded-full shadow-md shadow-yellowColor' src={person_1} alt='per1'/>
         </div>
     </div>
   )
