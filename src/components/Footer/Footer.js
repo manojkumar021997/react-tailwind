304e6d4edc3341f6b485cb7276e864c9a9bebbde
Arunprakash <arunprakash7871@gmail.com>
2023-05-09 16:49:07 +0530

    completed footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react'
+// import {IoCall} from 'react-icons/io5'
+// import {IoMail} from 'react-icons/io5'
+// import {MdLocationPin} from 'react-icons/md'
+// import {BsFacebook} from 'react-icons/bs'
+// import {AiFillInstagram} from 'react-icons/ai'
+// import {AiFillGithub} from 'react-icons/ai'
+// import {AiFillTwitterCircle} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+      <div className='grid grid-cols-5 p-[1rem] items-center'>
+          <div>
+            <strong className="text-[25px] font-semibold text-yellowColor">Bumble</strong>
+            <span className="text-[25px] text-shdowColor opacity-70 font-semibold">bee</span>
+          </div>
+          <div className='text-shdowColor opacity-70 hover:opacity-100 flex flex-col gap-4'>
+            <h3 className='text-shdowColor text-[20px] font-semibold opacity-70 hover:opacity-100 hover:text-yellowColor'>Company</h3>
+            <p>A hiring Company</p>
+            <p>Product Based</p>
+            <p>Contact us</p>
+          </div>
+          <div className='text-shdowColor font-semibold opacity-70 hover:opacity-100 flex flex-col gap-4 '>
+            <h3 className='text-[20px] hover:text-yellowColor'>Privacy</h3>
+            <p>Privacy Policy</p>
+            <p>Information</p>
+            <p>Additional Value</p>
+          </div>
+          <div className='text-shdowColor  font-semibold opacity-70 hover:opacity-100 flex flex-col gap-4'>
+            <h3 className='text-[20px] hover:text-yellowColor'>Training</h3>
+            <p>Privacy Policy</p>
+            <p>Information</p>
+            <p>Additional Value</p>
+          </div>
+          <div className='text-shdowColor   opacity-70 hover:opacity-100 flex flex-col gap-4'>
+            <h3 className='font-semibold hover:text-yellowColor'>Work</h3>
+            <p>FrontEnd Developer</p>
+            <p>BackEnd Developer</p>
+            <p>Network Engineer</p>
+            <p>Tester</p>
+              {/* <div>
+                  <h1 className="font-bold  text-2xl tracking-wide text-yellowColor">Contact Us</h1>
+                  <p className="pt-2 text-shdowColor text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia assumenda pariatur natus expedita vitae minima</p>
+              </div>
+              <div></div>
+              <div className=" flex">
+                  <div className='text-yellowColor'><IoCall /></div>
+                  <span className='text-shdowColor'>+(123) 9876 5432</span>
+              </div>
+
+              <div className=" flex">
+                  <div className='text-yellowColor'><IoMail /></div>
+                  <span className='text-shdowColor'>manojkumar0209@gmail.com</span>
+              </div>
+
+              <div className=" flex">
+                  <div className='text-yellowColor'><MdLocationPin /></div>
+                  <span className='text-shdowColor'>11,Street 342, Abcd fgh</span>
+              </div>
+              <div className=' flex text-yellowColor space-x-4 text-xl '>
+                  <a href="a" className='hover:text-shdowColor'><BsFacebook /></a>
+                  <a href="a" className='hover:text-shdowColor'><AiFillInstagram /></a>
+                  <a href="a" className='hover:text-shdowColor'><AiFillGithub /></a>
+                  <a href="a" className='hover:text-shdowColor'><AiFillTwitterCircle /></a>
+              </div> */}
+          </div>
+      </div>
+  )
+}
+
+export default Footer
--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -1,10 +1,12 @@
 import Card from "../components/Card/Card"
 import JobCard from "../components/JobCard/JobCard"
+import Footer from "../components/Footer/Footer"
 const Home = () =>{
     return(
        <>
        <Card/>
        <JobCard/>
+       <Footer/>
        </>
     )
 }
