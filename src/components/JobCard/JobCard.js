a8f4af4fd88b4fbb0a63b40eb61e7e6a90471d71
Arunprakash <arunprakash7871@gmail.com>
2023-05-09 15:29:39 +0530

    created jobcard component

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+// import {MdOutlineWorkHistory} from 'react-icons/md'
 
 const JobCard = () => {
     const UserDetails = [
       {
         id: 1,
         Work: "FrontEnd Developer",
+        'image':'https://e7.pngegg.com/pngimages/876/605/png-clipart-front-end-web-development-web-design-front-and-back-ends-web-developer-web-design-web-design-text.png',
         Employees: "6",
         description: "Front end developers for Websites",
       },
@@ -27,22 +29,37 @@ const JobCard = () => {
         description: "Front end developers for Websites",
       },
       {
-        id: 3,
+        id: 5,
         Work: "Tech-Support",
         Employees: "3",
         description: "Front end developers for Websites",
       },
+      {
+        id: 6,
+        Work: "Tester",
+        Employees: "1",
+        description: "Tester For web App",
+      },
     ];
   return (
-    <div className='flex flex-wrap gap-[1rem] text-shdowColor p-[2rem]'>
+    <div className='flex flex-wrap gap-[1rem] p-[2rem]'>
       {UserDetails.map((val)=>{
         return(
-            <div className=' flex flex-col items-start justify-center h-[120px] w-[30%] p-[1rem] hover:bg-yellowColor hover:text-blackColor'>
-
-                <p className='bg-transparent'>Work: {val.Work}</p>
-                <p className='bg-transparent'>Description: {val.description}</p>
-                <span className='bg-transparent'>Total Employees: {val.Employees}</span>
+          <div className=' flex flex-col items-center bg-whiteColor rounded-lg text-blackColor font-medium justify-between h-[240px] w-[30%] p-[1rem] hover:bg-blackColor hover:text-yellowColor'>
+            <div className='bg-transparent w-[100%] flex items-center justify-center'>
+              <p className='bg-transparent font-semibold flex items-center text-[1.1rem]'>{val.Work}</p>
             </div>
+            <div className='bg-transparent'>
+              <p className='bg-transparent flex items-center flex-col'>
+                <span className='bg-transparent'>Description:</span> {val.description}
+              </p>
+              <p className='bg-transparent flex items-center flex-col'>
+                <span className='bg-transparent'>Total Employees:</span>
+                <span className='text-[25px] bg-transparent  rounded-full'>{val.Employees}</span>
+              </p>
+            </div>
+            <button className='border-2 border-blackColor hover:text-shdowColor hover:border-shdowColor p-2'>View Details</button>
+          </div>
         )
       })}
     </div>
